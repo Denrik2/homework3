7f3ace7b23fabbf3e77d1c28c8bc81e28d62bdc1
Denrik <ketovdany@yandex.ru>
2024-09-06 00:14:39 +0400

    дз3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание №1
+        int cat = 123123;
+        byte dog = 123;
+        short shark = 1231;
+        long frog = 12312312;
+        float box = 123.1231f;
+        double one = 123.123123123123f;
+        System.out.println("Задача №1");
+        System.out.println("Значение переменной cat с типом int равно " + cat);
+        System.out.println("Значение переменной dog с типом byte равно " + dog);
+        System.out.println("Значение переменной shark с типом short равно " + shark);
+        System.out.println("Значение переменной frog с типом long равно " + frog);
+        System.out.println("Значение переменной box с типом float равно " + box);
+        System.out.println("Значение переменной one с типом double равно " + one);
+
+        //Задание №2
+        float catDog = 27.12f;
+        long dogCat = 987678965549L;
+        String numberCat = "2,786";
+        short numberDog = 569;
+        short numberShark = -159;
+        short dtps = 27897;
+        byte dps = 67;
+
+        //Задание №3
+
     }
 }
